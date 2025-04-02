f750967c5d71c4a8e7cb80684511a9458a53ec65
keemcc <ktsmccann@gmail.com>
2025-04-01 21:56:42 -0500

    Created StandardMidiEventFactory and implemented methods

---
+++ CS3330_KeehinMcCann_Assignment_3/src/mylittlemozart/midievent/StandardMidiEventFactory.java
@@ -0,0 +1,23 @@
+package mylittlemozart.midievent;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
+
+public class StandardMidiEventFactory implements MidiEventFactory{
+
+	@Override
+	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
+		ShortMessage message = new ShortMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+		MidiEvent event = new MidiEvent(message, tick);
+		return event;
+	}
+
+	@Override
+	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
+		ShortMessage message = new ShortMessage(ShortMessage.NOTE_OFF, channel, note, 0);
+		MidiEvent event = new MidiEvent(message, tick);
+		return event;
+	}
+
+}
