8c5e0246aa1978dade9756da4aedc072ffa7d4c6
keemcc <ktsmccann@gmail.com>
2025-04-01 19:25:06 -0500

    Created MidiEventData class with given fields and methods

--- CS3330_KeehinMcCann_Assignment_3/src/mylittlemozart/csvparser/MidiEventData.java
+++ CS3330_KeehinMcCann_Assignment_3/src/mylittlemozart/csvparser/MidiEventData.java
@@ -1,5 +1,15 @@
 package mylittlemozart.csvparser;
 
 public class MidiEventData {
-
+	private int startEndTick, velocity, note, channel, noteOnOff;
+	private int instrument;
+	
+	public MidiEventData(int startEndTick, int velocity, int note, int channel, int instrument, int noteOnOff) {
+		this.startEndTick = startEndTick;
+		this.velocity = velocity;
+		this.note = note;
+		this.channel = channel;
+		this.instrument = instrument;
+		this.noteOnOff = noteOnOff;
+	}
 }
