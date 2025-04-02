04ed8d1a3587eb8bb704e56f2f2a155d67dc233e
keemcc <ktsmccann@gmail.com>
2025-04-01 19:25:39 -0500

    Created MidiCsvParser class with given field

---
+++ CS3330_KeehinMcCann_Assignment_3/src/mylittlemozart/csvparser/MidiCsvParser.java
@@ -0,0 +1,11 @@
+package mylittlemozart.csvparser;
+
+import java.util.ArrayList;
+
+public class MidiCsvParser {
+	private ArrayList<MidiEventData> midiList;
+	
+	public MidiCsvParser() {
+		
+	}
+}
