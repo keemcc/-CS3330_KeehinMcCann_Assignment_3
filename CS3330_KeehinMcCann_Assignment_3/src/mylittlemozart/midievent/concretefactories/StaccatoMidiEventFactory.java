214c3810a45a3b8873e52e092d1a710bef400e5c
keemcc <ktsmccann@gmail.com>
2025-04-02 23:38:13 -0500

    Added JavaDocs to StaccatoMidiEventFactory

--- CS3330_KeehinMcCann_Assignment_3/src/mylittlemozart/midievent/concretefactories/StaccatoMidiEventFactory.java
+++ CS3330_KeehinMcCann_Assignment_3/src/mylittlemozart/midievent/concretefactories/StaccatoMidiEventFactory.java
@@ -4,6 +4,9 @@ import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.ShortMessage;
 
+/**
+ * This class implements MidiEventFactory and creates staccato notes, this is done by subtracting 120 ticks from the end of the note.
+ */
 public class StaccatoMidiEventFactory implements MidiEventFactory{
 
 	@Override
@@ -16,6 +19,7 @@ public class StaccatoMidiEventFactory implements MidiEventFactory{
 	@Override
 	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
 		ShortMessage message = new ShortMessage(ShortMessage.NOTE_OFF, channel, note, 0);
+		//Subtract 120 ticks from the note off so that it is staccato.
 		MidiEvent event = new MidiEvent(message, (tick - 120));
 		return event;
 	}
