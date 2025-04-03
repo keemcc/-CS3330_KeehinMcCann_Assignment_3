f3b2da4dfcc071015c6d25d609af79e3e669f911
keemcc <ktsmccann@gmail.com>
2025-04-03 00:58:50 -0500

    added javadocs to InstrumentStrategy

--- CS3330_KeehinMcCann_Assignment_3/src/mylittlemozart/midievent/strategies/instrumentstrategy/InstrumentStrategy.java
+++ CS3330_KeehinMcCann_Assignment_3/src/mylittlemozart/midievent/strategies/instrumentstrategy/InstrumentStrategy.java
@@ -2,6 +2,14 @@ package mylittlemozart.midievent.strategies.instrumentstrategy;
 
 import javax.sound.midi.Track;
 
+/**
+ * This interface contains a method to apply different instruments given a track and a channel
+ */
 public interface InstrumentStrategy {
+	/**
+	 * Applies the instrument to the passed track and channel
+	 * @param track track to apply to
+	 * @param channel channel to apply to
+	 */
 	void applyInstrument(Track track, int channel);
 }
