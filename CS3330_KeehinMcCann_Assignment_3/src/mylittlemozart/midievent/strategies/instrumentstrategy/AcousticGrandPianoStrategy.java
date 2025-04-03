e8707907c85c1f1b44567e03f1d04243f9e70158
keemcc <ktsmccann@gmail.com>
2025-04-03 01:25:15 -0500

    Added java docs to instrument strategy implementations

--- CS3330_KeehinMcCann_Assignment_3/src/mylittlemozart/midievent/strategies/instrumentstrategy/AcousticGrandPianoStrategy.java
+++ CS3330_KeehinMcCann_Assignment_3/src/mylittlemozart/midievent/strategies/instrumentstrategy/AcousticGrandPianoStrategy.java
@@ -5,10 +5,14 @@ import javax.sound.midi.MidiEvent;
 import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Track;
 
+/**
+ * This class implements the instrument strategy to apply an acoustic grand piano sound to the passed track and channel
+ */
 public class AcousticGrandPianoStrategy implements InstrumentStrategy{
 
 	@Override
 	public void applyInstrument(Track track, int channel) {
+		//Try to apply the instrument. If an error occurs, print stack trace
 		try {
 			ShortMessage message = new ShortMessage(ShortMessage.PROGRAM_CHANGE, channel, 0, 0);
 			MidiEvent event = new MidiEvent(message, 0);
--- CS3330_KeehinMcCann_Assignment_3/src/mylittlemozart/midievent/strategies/instrumentstrategy/ElectricBassGuitarStrategy.java
+++ CS3330_KeehinMcCann_Assignment_3/src/mylittlemozart/midievent/strategies/instrumentstrategy/ElectricBassGuitarStrategy.java
@@ -5,10 +5,14 @@ import javax.sound.midi.MidiEvent;
 import javax.sound.midi.ShortMessage;
 import javax.sound.midi.Track;
 
+/**
+ * This class implements the instrument strategy to apply an electric bass guitar sound to the passed track and channel
+ */
 public class ElectricBassGuitarStrategy implements InstrumentStrategy {
 
 	@Override
 	public void applyInstrument(Track track, int channel) {
+		//Try to apply the instrument. If an error occurs, print stack trace
 		try {
 			ShortMessage message = new ShortMessage(ShortMessage.PROGRAM_CHANGE, channel, 33, 0);
 			MidiEvent event = new MidiEvent(message, 0);
