cf98f90c3aee4c4fe024c5233ecc5fdedc88f60d
keemcc <ktsmccann@gmail.com>
2025-04-02 23:26:43 -0500

    Added java docs to LegatoMidiEventFactory

--- CS3330_KeehinMcCann_Assignment_3/src/mylittlemozart/midievent/concretefactories/LegatoMidiEventFactory.java
+++ CS3330_KeehinMcCann_Assignment_3/src/mylittlemozart/midievent/concretefactories/LegatoMidiEventFactory.java
@@ -4,6 +4,9 @@ import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.ShortMessage;
 
+/**
+ * This class implements the MidiEventFactory to create legato notes, this is done by adding 80 ticks to the node off event
+ */
 public class LegatoMidiEventFactory implements MidiEventFactory{
 
 	@Override
@@ -16,7 +19,8 @@ public class LegatoMidiEventFactory implements MidiEventFactory{
 	@Override
 	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
 		ShortMessage message = new ShortMessage(ShortMessage.NOTE_OFF, channel, note, 0);
-		MidiEvent event = new MidiEvent(message, tick + 80);
+		//Add 80 ticks to the note off to create a legato feel
+		MidiEvent event = new MidiEvent(message, (tick + 80));
 		return event;
 	}
 }
