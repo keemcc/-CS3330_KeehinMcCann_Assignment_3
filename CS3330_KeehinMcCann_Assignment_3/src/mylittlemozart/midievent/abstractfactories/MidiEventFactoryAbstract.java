d1bde683945a11029c6cb188deafb40f77a2bb76
keemcc <ktsmccann@gmail.com>
2025-04-03 01:00:54 -0500

    Added more javadocs to abstract factory

--- CS3330_KeehinMcCann_Assignment_3/src/mylittlemozart/midievent/abstractfactories/MidiEventFactoryAbstract.java
+++ CS3330_KeehinMcCann_Assignment_3/src/mylittlemozart/midievent/abstractfactories/MidiEventFactoryAbstract.java
@@ -6,5 +6,9 @@ import mylittlemozart.midievent.concretefactories.MidiEventFactory;
  * This interface provides the method createFactory to instantiate a MidiEventFactory object.
  */
 public interface MidiEventFactoryAbstract {
+	/**
+	 * Creates a factory
+	 * @return factory object
+	 */
 	MidiEventFactory createFactory();
 }
